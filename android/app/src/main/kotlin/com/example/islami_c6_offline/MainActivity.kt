3f4623bf090ba7edcaaf87d147613a249cf59362
eCommerceAPP <mohamedhamouda9879>
2022-07-15 12:07:45 +0200

    init git empty screen

---
+++ android/app/src/main/kotlin/com/example/islami_c6_offline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.islami_c6_offline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:islami_c6_offline/home_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeLayout.routeName,
+      routes: {
+        HomeLayout.routeName: (c) => HomeLayout(),
+      },
+    );
+  }
+}
